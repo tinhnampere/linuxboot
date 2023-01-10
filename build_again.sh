72543e3c90473c293c160b9268a57784117f58ec
Tinh Nguyen <tinhn@amperecomputing.com>
2023-01-10 22:35:51 +0700

    Support build script for jade
    Signed-off-by: Tinh Nguyen <tinhn@amperecomputing.com>

---
+++ build_again.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+export WORKSPACE=${PWD}
+TOOLCHAINS=${WORKSPACE}/toolchain
+PLATFORM_MAKE_DIR=${WORKSPACE}/mainboards/ampere/jade
+
+# set env
+export PATH=${TOOLCHAINS}/go/bin:${TOOLCHAINS}/bin:$PATH
+export PATH=${WORKSPACE}/u-root:$PATH
+export GOPATH=${TOOLCHAINS}
+export GOENV=${TOOLCHAINS}/.config/go/env
+export GOCACHE=${TOOLCHAINS}/.cache/go-build
+
+#clear old-output
+rm -rf ${PLATFORM_MAKE_DIR}/flashkernel
+rm -rf ${PLATFORM_MAKE_DIR}/flashinitramfs*
+
+"${CROSS_COMPILE}"gcc --version || exit 1
+make -C ${PLATFORM_MAKE_DIR} flashkernel ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE}
+if [ $? -ne 0 ]; then
+    echo "build kernel fail"
+else
+    echo "done"
+fi
